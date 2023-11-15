f764407604de165d55f5096921ab770210b02e7f
Shinzi <shinzisaja@gmail.com>
2023-11-15 15:11:45 +0700

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,30 +12,130 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-
+      home: ScrollAbleScreen(),
     );
   }
 }
 
+class ScrollAbleScreen extends StatelessWidget {
+  const ScrollAbleScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 50,
+            color: Colors.red,
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {},
+                ),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 130,
+            margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.network(
+                    'https://brandlogos.net/wp-content/uploads/2012/10/godrej-logo-vector.png',
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 20),
+                    ),
+                    const Text("Godrej"),
+                    Container(
+                      margin: const EdgeInsets.only(top: 5),
+                    ),
+                    const Row(
+                      children: [
+                        Icon(Icons.person),
+                        Text(
+                          "5.441 Pengikut",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 5),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          margin: const EdgeInsets.only(right: 10),
+                          height: 30,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "Mengikuti",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                        ),
+                        const Icon(Icons.share)
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(margin: EdgeInsets.only(top: 25),),
+          CarouselSlider(
+            
+            options: CarouselOptions(height: 100.0),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(color: Colors.amber, borderRadius: BorderRadius.circular(10)),
+                      );
+                },
+              );
+            }).toList(),
+          ),
+          Container(margin: EdgeInsets.only(top: 10,left: 5),child: Text("Belanja berdasarkan kategori", style: TextStyle(fontSize: 20),),),
 
+          
+        ],
+      ),
+    );
+  }
+}
